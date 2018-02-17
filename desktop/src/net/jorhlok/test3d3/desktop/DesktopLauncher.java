9f27bd15257fff94f0bde40a67066f5f0f1338d3
Jorhlok <joshmbrown42@gmail.com>
2018-02-17 16:23:32 -0600

    Initial commit

---
+++ desktop/src/net/jorhlok/test3d3/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.jorhlok.test3d3.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.jorhlok.test3d3.Test3D3;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Test3D3(), config);
+	}
+}
